dd3ca4619fd72aacfd7d385d6f182098339ecb44
Victor Yeo <victoryeo1974@gmail.com>
2020-12-12 16:58:46 +0800

    add test case to Home component

--- __tests__/uitest.spec.js
+++ __tests__/uitest.spec.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, screen, queryByAttribute } from '@testing-library/react'
+import "@testing-library/jest-dom/extend-expect"
 import Title from '../pages/title'
-import Index from '../pages/index'
+import Home from '../pages/index'
 
-// for testing-library
-test('loads and displays ui', async ()=> {
+// use testing-library
+describe('Home', () => {
+  test('displays Edit button', async ()=> {
+    render(<Home/>)
+    const titles = screen.getAllByRole('button')
+    expect(titles[0]).toHaveTextContent('Edit')
+  })
+  test('displays Clear button', async ()=> {
+    render(<Home/>)
+    const titles = screen.getAllByRole('button')
+    expect(titles[1]).toHaveTextContent('Clear')
+  })
+  test('click Edit button to remove readonly attribute', async ()=> {
+    const getById = queryByAttribute.bind(null, 'id')
+    const dom = render(<Home/>)
+    fireEvent.click(screen.getByText('Edit'))
+    const inputs = getById(dom.container, 'tsmonthlite')
+    expect(inputs.hasAttribute('readOnly')).toBeFalsy()
+  })
 })
 
-describe('Title component', () => {
+describe('Dummy title component', () => {
   it('Should render', () => {
-    const wrapper = mount(<Title label="test" />);
-
-    expect(wrapper.find('h1')).toHaveLength(1);
+    const wrapper = mount(<Title label="test" />)
+    expect(wrapper.find('h1')).toHaveLength(1)
   })
 })
