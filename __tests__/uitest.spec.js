707e395bd2d66970c8b7255f58e91c0de3679a43
Victor Yeo <victoryeo1974@gmail.com>
2020-12-12 14:49:41 +0800

    add ui test

--- __tests__/uitest.spec.js
+++ __tests__/uitest.spec.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import testing from '@testing-library/react'
-const { render, fireEvent } = testing
+import { mount } from 'enzyme'
+import { render, fireEvent } from '@testing-library/react'
+import Title from '../pages/title'
+import Index from '../pages/index'
 
+// for testing-library
 test('loads and displays ui', async ()=> {
+})
+
+describe('Title component', () => {
+  it('Should render', () => {
+    const wrapper = mount(<Title label="test" />);
+
+    expect(wrapper.find('h1')).toHaveLength(1);
+  })
 })
